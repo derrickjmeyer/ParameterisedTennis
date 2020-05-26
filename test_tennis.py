a51c0a62d954e3b5436c03dda1bac425236cb461
derrickjmeyer <derrick.meyer@jamf.com>
2020-05-26 12:13:35 +1000

    Add parameterised pytest

--- test_tennis.py
+++ test_tennis.py
@@ -1,5 +1,15 @@
+import pytest
+
 from tennis import score_tennis
 
+
+@pytest.mark.parametrize("player1_points, player2_points, expected_score",
+                         [(0, 0, "Love-All"),
+                          (1, 1, "Fifteen-All"),
+                          (2, 2, "Thirty-All")
+                          ])
+def test_score_tennis(player1_points, player2_points, expected_score):
+    assert score_tennis(player1_points, player2_points) == expected_score
 
 def test_0_0_love_all():
     assert score_tennis(0, 0) == "Love-All"
