a9dc29486efe84d42de02be62d87164d7fee4d4b
derrickjmeyer <derrick.meyer@jamf.com>
2020-05-26 12:24:47 +1000

    Add unittest parameterized test

--- test_tennis_unittest.py
+++ test_tennis_unittest.py
@@ -5,6 +5,16 @@ from tennis import score_tennis
 
 class TennisTest(unittest.TestCase):
 
+    def test_score_tennis(self):
+        test_cases = [
+            (0, 0, "Love-All"),
+            (1, 1, "Fifteen-All"),
+            (2, 2, "Thirty-All"),
+        ]
+        for player1_points, player2_points, expected_score in test_cases:
+            with self.subTest(f"{player1_points}, {player2_points} -> {expected_score}"):
+                self.assertEqual(expected_score, score_tennis(player1_points, player2_points))
+
     def test_0_0_love_all(self):
         self.assertEqual("Love-All", score_tennis(0,0))
 
