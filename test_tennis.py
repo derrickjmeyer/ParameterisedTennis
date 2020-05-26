55e4d075dca21a0b954410f63bbeb0b1445d9784
derrickjmeyer <derrick.meyer@jamf.com>
2020-05-26 12:14:02 +1000

    Remove old individual pytests

--- test_tennis.py
+++ test_tennis.py
@@ -10,12 +10,3 @@ from tennis import score_tennis
                           ])
 def test_score_tennis(player1_points, player2_points, expected_score):
     assert score_tennis(player1_points, player2_points) == expected_score
-
-def test_0_0_love_all():
-    assert score_tennis(0, 0) == "Love-All"
-
-def test_1_1_fifteen_all():
-    assert score_tennis(1,1) == "Fifteen-All"
-
-def test_2_2_thirty_all():
-    assert score_tennis(2, 2) == "Thirty-All"
